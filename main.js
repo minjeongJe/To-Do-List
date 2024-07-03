782818142ddbc28af59eb6e2ae3eb882a2ab3cfe
minjeongJe <zzssjj@naver.com>
2024-07-03 15:06:41 +0900

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+ // 유저가 값을 입력한다.
+ // + 버튼을 클릭하면, 할일이 추가된다.
+ // delete 버튼을 누르면 할일이 삭제된다.
+ // check 버튼을 누르면 할일이 끝나면서 밑줄이 간다.
+ // 1. check 버튼을 클릭하는 순간 true false
+ // 2. ture이면 끝난걸로 간주하고 밑줄 보여주기
+ // 3. false이면 안끝난걸로 간주하고 그대로
+
+ // 진행중 끝남 탭을 누르면, 언더바가 이동한다.
+ // 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만 보여지게 한다.
+ // 전체탭을 누르면 다시 전체아이템으로 돌아온다.
+
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList = [];
@@ -24,26 +36,65 @@ taskInput.addEventListener("click", function() {
 })
 
 function addTask () {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id: randomIDGenerate (),
+        taskContent: taskInput.value,
+        isComplete: false,
+    }
+    taskList.push(task);
     console.log(taskList);
     render();
 }
 
 function render() {
     let resultHTML = "";
-    for (let i = 0; i < taskList.length; i++) {
-        resultHTML += `
-            <div class="task">
-                <div>${taskList[i]}</div>
+    for(let i = 0; i < taskList.length; i++) {
+        if(taskList[i].isComplete == true) {
+            resultHTML += `
+            <div class="task task-done">
+                <div class="taskList">${taskList[i].taskContent}</div>
                 <div>
-                    <button>Check</button>
-                    <button>Delete</button>
+                    <button onclick = "toggleComplete('${taskList[i].id}')"><i class="material-icons check">restart_alt</i></button>
+                    <button onclick = "deleteTask('${taskList[i].id}')"><i class="material-icons delete">delete_forever</i></button>
                 </div>
-            </div>
-        `;
+            </div> `;
+        }else {
+             resultHTML += `
+            <div class="task">
+                <div class="taskList">${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick = "toggleComplete('${taskList[i].id}')"><i class="material-icons check">check_circle</i></button>
+                    <button onclick = "deleteTask('${taskList[i].id}')"><i class="material-icons delete">delete_forever</i></button>
+                </div>
+            </div> `;
+        }
     }
-
     document.getElementById("task-board").innerHTML = resultHTML;
 }
+
+function toggleComplete (id) {
+    for(let i = 0; i < taskList.length; i++) {
+        if(taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+};
+
+function deleteTask(id) {  
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id === id) {
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    console.log(taskList);
+    render();
+}
+
+function randomIDGenerate () {
+    return '_' + Math.random().toString(36).substr(2,9);
+};
 
